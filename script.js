06832f6cfdcd110855892c5db5a488d1137aa311
Justin Gordon <linuxfreak1030@gmail.com>
2025-03-09 21:10:59 -0400

    refactor array options to use all upper-case letters.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     // Create an array variable called options that contains three strings ("Rock","Paper","Scissors")
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["ROCK", "PAPER", "SCISSORS"];
     // Create a variable called randomNum of type number that will store a random number between 0 and 2 inclusive
     let randomNum = Math.floor(Math.random() * 3)
     // return the options array item that corresponds to randomNum
